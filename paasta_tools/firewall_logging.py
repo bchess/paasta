b7f00b239bceed6df84e17c020d13f687db5dac0
Benjamin Chess <bchess@yelp.com>
2017-06-06 06:38:39 -0700

    wip

--- paasta_tools/firewall_logging.py
+++ paasta_tools/firewall_logging.py
@@ -1,43 +1,102 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import argparse
+import logging
+import sys
+
 from six.moves import socketserver
 import syslogmp
+
+from paasta_tools.firewall import services_running_here
+
+
+log = logging.getLogger(__name__)
+
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data, socket = self.request
 
-        iptables_log = parse_syslog(data)
+        syslog_to_scribe(data)
+
+
+def syslog_to_scribe(data):
+    iptables_log = parse_syslog(data)
+    service, instance = lookup_service_instance_by_ip(iptables_log['SRC'])
+    if service is None:
+        return
+
+    ScribeLogWriter().log(
+        service,
+        iptables_log['message'],
+        'security',
+        'warning',
+        #cluster=TODO,
+        instance=instance,
+    )
 
 
 def parse_syslog(data):
     parsed_data = syslogmp.parse(data)
-    message = parsed_data.message
-    if not message.startswith('kernel: ['):
+    full_message = parsed_data.message
+    if not full_message.startswith('kernel: ['):
         # Not a kernel message
         return None
 
-    parts = message.strip().split(' ')
-    # parts[0] is 'kernel: '
-    # parts[1] is timestamp from boot
-    # parts[2] is the log-prefix
-    # parts[3..] is either KEY=VALUE or just KEY
-    if not parts[3].startswith('IN='):
+    close_bracket = full_message.find(']')
+    if close_bracket == -1:
+        return None
+
+    iptables_message = full_message[close_bracket:].strip()
+    parts = iptables_message.split(' ')
+    # parts[0] is the log-prefix
+    # parts[1..] is either KEY=VALUE or just KEY
+    if not parts[1].startswith('IN='):
         # not an iptables message
         return None
 
-    fields = {k: v for k, _, v in (field.partition('=') for field in parts[3:])}
-    fields['prefix'] = parts[2]
+    fields = {k: v for k, _, v in (field.partition('=') for field in parts[1:])}
+    fields['prefix'] = parts[0]
+    fields['message'] = iptables_message
     return fields
 
 
-def lookup_container_by_ip(ip_address):
+def lookup_service_instance_by_ip(ip_lookup):
+    for service, instance, mac, ip in services_running_here():
+        if ip == ip_lookup:
+            return (service, instance)
+    log.info('Unable to find container for ip {}'.format(ip_lookup))
+    return (None, None)
 
-def run_server():
-    server = socketserver.UDPServer(('0.0.0.0', 1226), SyslogUDPHandler)
+
+def run_server(listen_host, listen_port):
+    logging.basicConfig(level=logging.DEBUG)
+    server = socketserver.UDPServer((listen_host, listen_port), SyslogUDPHandler)
     server.serve_forever()
 
 
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description='Adapts iptables syslog messages into scribe')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        dest="verbose", default=False)
+    parser.add_argument('-l', '--listen-host', help='Default %(default)s', default='0.0.0.0')
+    parser.add_argument('-p', '--listen-port', type=int, help='Default %(default)s', default=1516)
+    args = parser.parse_args(argv)
+    return args
+
+
+def setup_logging(verbose):
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    setup_logging(args.verbose)
+    run_server(args.listen_host, args.listen_port)
+
 if __name__ == '__main__':
-    run_server()
+    main()
